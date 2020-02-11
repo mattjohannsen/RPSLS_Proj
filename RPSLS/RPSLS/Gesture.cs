20b1210b1421d3e32d0279728b7d216525070c06
matt johannsen <majohann@hotmail.com>
2020-02-11 13:16:52 -0600

    Added Outcome class, and added member variables for Outcome and Gesture class

---
+++ RPSLS/RPSLS/Gesture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    public class Gesture
+    {
+        //member methods
+        string gestureName;
+
+        // constructor
+        public Gesture(string gestureName)
+        {
+            this.gestureName = gestureName;
+        }
+
+        // member variables
+
+
+    }
+}
---
+++ RPSLS/RPSLS/Outcome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Outcome
+    {
+        bool outcome0;
+        bool outcome1;
+        bool outcome2;
+        bool outcome3;
+        bool outcome4;
+
+        public Outcome(bool outcome0, bool outcome1, bool outcome2, bool outcome3, bool outcome4)
+        {
+            this.outcome0 = outcome0;
+            this.outcome1 = outcome1;
+            this.outcome2 = outcome2;
+            this.outcome3 = outcome3;
+            this.outcome4 = outcome4;
+        }
+    }
+}
