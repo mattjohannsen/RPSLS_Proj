61da2a1ac35e8a7d470d3e45fb7152164a056cd0
matt johannsen <majohann@hotmail.com>
2020-02-12 10:01:57 -0600

    Added GetNumberofPlayers and SetPlayers methods.

--- RPSLS/RPSLS/Game.cs
+++ RPSLS/RPSLS/Game.cs
@@ -11,7 +11,7 @@ namespace RPSLS
         // member variables
         Player player1;
         Player player2;
-
+        // This is where the List<string> gestures will go
 
 
 
@@ -26,9 +26,32 @@ namespace RPSLS
         // member methods
         public void RunGame()
         {
+            //This is where the method will go to show the instructions
             Console.WriteLine("Rock Paper Scissors Lizzard Spock");
+
         }
 
+        private string GetNumberOfPlayers()
+        {
+            Console.WriteLine("Are you playing with 1 or 2 players?");
+            string numberPlayers = Console.ReadLine();
+            return numberPlayers;
+        }
+
+        private void SetPlayers(string numberofPlayers)
+        {
+            if (numberofPlayers == "1")
+            {
+                player1 = new Human();
+                player2 = new AI();
+            }
+            else if (numberofPlayers == "2")
+            {
+                player1 = new Human();
+                player2 = new Human();
+            }
+
+        }
 
     }
 }
